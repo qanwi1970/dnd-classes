b2fa5f043f0c5108003b52d7375ebecddb733cbd
William DeLude <qanwi1970@gmail.com>
2016-02-12 23:04:12 -0700

    Initial project

---
+++ src/main/java/dungeonmart/ClassesApplication.java
@@ -0,0 +1,12 @@
+package dungeonmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassesApplication.class, args);
+	}
+}
---
+++ src/test/java/dungeonmart/ClassesApplicationTests.java
@@ -0,0 +1,18 @@
+package dungeonmart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClassesApplication.class)
+@WebAppConfiguration
+public class ClassesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
